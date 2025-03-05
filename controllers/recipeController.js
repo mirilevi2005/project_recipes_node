724f59319131bbcdab35f8e5c69986830aced91b
מרים לוי <MiryamLe@smg.local>
2025-03-05 19:56:02 +0200

    getRecipeById

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -9,7 +9,19 @@ exports.getAllRecipes = async (req, res) => {
         res.status(500).json({ message: 'Failed to get recipe' });
       }
 };
-exports.addRecipe = (req, res) => {};
-exports.deleteRecipe = (req, res) => {};
-exports.updateRecipe = (req, res) => {};
+
+exports.getRecipeById = async (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  try {
+    const recipe = await Recipe.findOne({ id });
+    if (!recipe) {
+      return res.status(404).json({ message: 'recipe not found' });
+    }
+    res.json(recipe);
+  } catch (error) {
+    console.error('Failed to get recipe:', error);
+    res.status(500).json({ message: 'Failed to get recipe' });
+  }
+};
 
