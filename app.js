154eebce81245ccf57c9a09c759fa68443e55534
מרים לוי <MiryamLe@smg.local>
2025-03-05 18:48:10 +0200

    addRecipe

--- app.js
+++ app.js
@@ -7,18 +7,23 @@ const connectDB = require("./config/dbConn")
 const PORT = process.env.PORT || 7001
 const app = express()
 connectDB()
+const resipeRouter =require('./recipeRouter')
+
 //middlewares
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.static("public"))
+
 //routes
 app.get("/", (req, res) => {
     res.send("this is the home page")
 })
+
 // app.listen(PORT, () => {
 //     console.log(`Server running on port ${PORT}`)
 //     console.log(process.env.NODE_ENV)
 // })
+ app.use('./recipeRouter', resipeRouter);
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
     app.listen(PORT, () => console.log(`Server running on port
