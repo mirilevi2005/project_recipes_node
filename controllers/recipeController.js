14b23c4870d5568c1ea3070a9abaea944fa4612a
מרים לוי <MiryamLe@smg.local>
2025-03-05 17:34:59 +0200

    addRecipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -3,5 +3,4 @@ exports.addRecipe = async (req, res) => {
     const resipe = await Resipe.create(req.body);
     res.json(resipe)
 };
-exports.deleteRecipe = (req, res) => {};
-exports.updateRecipe = (req, res) => {};
+
